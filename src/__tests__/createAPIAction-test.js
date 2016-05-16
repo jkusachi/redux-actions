8697ff3322cfef6018d1f21ad8aca54f736f0f21
James Kusachi <jamesku@zillow.com>
2016-05-16 14:55:22 -0700

    Updating createAPIAction to handle several verbs

--- src/__tests__/createAPIAction-test.js
+++ src/__tests__/createAPIAction-test.js
@@ -1,28 +1,25 @@
 import { createAPIAction } from '../';
 import { isFSA } from 'flux-standard-action';
 
-describe.only('createAPIAction()', () => {
+describe('createAPIAction()', () => {
   describe('resulting action creator', () => {
     const type = 'TYPE';
 
     it('returns a valid FSA', () => {
       const actionCreator = createAPIAction(type, 'GET', '/sample', b => b);
-      const foobar = { foo: 'bar' };
-      const action = actionCreator(foobar);
+      const action = actionCreator();
       expect(isFSA(action)).to.be.true;
     });
 
     it('uses return value as payload', () => {
       const actionCreator = createAPIAction(type, 'GET', '/sample', b => b, b => b);
-      const foobar = { foo: 'bar' };
-      const action = actionCreator(foobar);
+      const action = actionCreator();
       expect(action).to.deep.equal({
         type,
-        payload: foobar,
+        payload: {},
         meta: {
           api: true,
           method: 'GET',
-          foo: 'bar',
           endpoint: '/sample',
           types: [
             type.concat('_GET_REQUEST'),
@@ -34,7 +31,7 @@ describe.only('createAPIAction()', () => {
     });
 
     it('uses identity function if actionCreator is not a function', () => {
-      const actionCreator = createAPIAction(type, 'GET', '/sample');
+      const actionCreator = createAPIAction(type, 'POST', '/sample');
       const foobar = { foo: 'bar' };
       const action = actionCreator(foobar);
       expect(action).to.deep.equal({
@@ -42,12 +39,12 @@ describe.only('createAPIAction()', () => {
         payload: foobar,
         meta: {
           api: true,
-          method: 'GET',
+          method: 'POST',
           endpoint: '/sample',
           types: [
-            type.concat('_GET_REQUEST'),
-            type.concat('_GET_SUCCESS'),
-            type.concat('_GET_FAILURE')
+            type.concat('_POST_REQUEST'),
+            type.concat('_POST_SUCCESS'),
+            type.concat('_POST_FAILURE')
           ]
         }
       });
@@ -55,7 +52,7 @@ describe.only('createAPIAction()', () => {
     });
 
     it('accepts a second parameter for adding meta to object', () => {
-      const actionCreator = createAPIAction(type, 'GET', '/sample', null, ({ cid }) => ({ cid }));
+      const actionCreator = createAPIAction(type, 'POST', '/sample', null, ({ cid }) => ({ cid }));
       const foobar = { foo: 'bar', cid: 5 };
       const action = actionCreator(foobar);
       expect(action).to.deep.equal({
@@ -63,13 +60,13 @@ describe.only('createAPIAction()', () => {
         payload: foobar,
         meta: {
           api: true,
-          method: 'GET',
+          method: 'POST',
           endpoint: '/sample',
           cid: 5,
           types: [
-            type.concat('_GET_REQUEST'),
-            type.concat('_GET_SUCCESS'),
-            type.concat('_GET_FAILURE')
+            type.concat('_POST_REQUEST'),
+            type.concat('_POST_SUCCESS'),
+            type.concat('_POST_FAILURE')
           ]
         }
       });
@@ -99,11 +96,32 @@ describe.only('createAPIAction()', () => {
       });
       expect(isFSA(errAction)).to.be.true;
 
+      const actionCreatorPost = createAPIAction(type, 'POST', '/sample');
       const foobar = { foo: 'bar', cid: 5 };
-      const noErrAction = actionCreator(foobar);
+      const noErrAction = actionCreatorPost(foobar);
       expect(noErrAction).to.deep.equal({
         type,
         payload: foobar,
+        meta: {
+          api: true,
+          method: 'POST',
+          endpoint: '/sample',
+          types: [
+            type.concat('_POST_REQUEST'),
+            type.concat('_POST_SUCCESS'),
+            type.concat('_POST_FAILURE')
+          ]
+        }
+      });
+      expect(isFSA(noErrAction)).to.be.true;
+    });
+
+
+    it('test GET endoint, ALL elements', () => {
+      const getItems = createAPIAction(type, 'GET', '/sample', b => b, b => b);
+      expect(getItems()).to.deep.equal({
+        type,
+        payload: {},
         meta: {
           api: true,
           method: 'GET',
@@ -115,9 +133,83 @@ describe.only('createAPIAction()', () => {
           ]
         }
       });
-      expect(isFSA(noErrAction)).to.be.true;
     });
 
+    it('test GET by ID endpoint', () => {
+      const getItems = createAPIAction(type, 'GET', '/sample');
+      expect(getItems(9)).to.deep.equal({
+        type,
+        payload: {},
+        meta: {
+          api: true,
+          method: 'GET',
+          endpoint: '/sample/9',
+          types: [
+            type.concat('_GET_REQUEST'),
+            type.concat('_GET_SUCCESS'),
+            type.concat('_GET_FAILURE')
+          ]
+        }
+      });
+    });
+
+    it('test POST Data endpoint', () => {
+      const createItems = createAPIAction(type, 'POST', '/sample');
+      const postData = { name: 'james' };
+      expect(createItems(postData)).to.deep.equal({
+        type,
+        payload: postData,
+        meta: {
+          api: true,
+          method: 'POST',
+          endpoint: '/sample',
+          types: [
+            type.concat('_POST_REQUEST'),
+            type.concat('_POST_SUCCESS'),
+            type.concat('_POST_FAILURE')
+          ]
+        }
+      });
+    });
+
+    it('test PUT Data endpoint', () => {
+      const updateItems = createAPIAction(type, 'PUT', '/sample');
+      const postID = 10;
+      const postData = { name: 'james' };
+      expect(updateItems(postID, postData)).to.deep.equal({
+        type,
+        payload: postData,
+        meta: {
+          api: true,
+          method: 'PUT',
+          endpoint: '/sample/10',
+          types: [
+            type.concat('_PUT_REQUEST'),
+            type.concat('_PUT_SUCCESS'),
+            type.concat('_PUT_FAILURE')
+          ]
+        }
+      });
+    });
+
+    it('test DELETE Data endpoint', () => {
+      const deleteItems = createAPIAction(type, 'DELETE', '/sample');
+      const postID = 5;
+      expect(deleteItems(postID)).to.deep.equal({
+        type,
+        payload: {},
+        meta: {
+          api: true,
+          method: 'DELETE',
+          endpoint: '/sample/5',
+          types: [
+            type.concat('_DELETE_REQUEST'),
+            type.concat('_DELETE_SUCCESS'),
+            type.concat('_DELETE_FAILURE')
+          ]
+        }
+      });
+    });
 
   });
 });
